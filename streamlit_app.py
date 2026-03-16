c6301e9ef264e8eecb615ad76a81a5a3e13f49d5
Imara Nazar <54583747+maranaz@users.noreply.github.com>
2026-03-16 22:43:54 +1300

    Update streamlit_app.py
    Added nutrient info from smoothiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -37,5 +38,10 @@ if ingredients_list:
     if trim_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+# new section to display the nutrient information
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+sf_df = st.dataframe(st.text(smoothiefroot_response.json()), use_container_width=True)
+
     
     
